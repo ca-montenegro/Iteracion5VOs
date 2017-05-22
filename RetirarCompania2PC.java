c4674e65daa33d2016f1ccedb270cdc4fc240fb5
tf-venegas10 <tf.venegas10@uniandes.edu.co>
2017-05-21 19:34:24 -0500

    rentabilidad

--- RetirarCompania2PC.java
+++ RetirarCompania2PC.java
@@ -248,6 +248,178 @@ public class RetirarCompania2PC {
 		return funciones;
 		
 	}
+	
+	
+		public VORentabilidad consultarRentabilidad(String nombreCompania) {
+		VORentabilidad rentabilidad = new VORentabilidad();
+		
+		try {
+			UserTransaction utx = (UserTransaction) context.lookup("/UserTransaction");
+			try{
+			iniciarConexiones();
+			utx.begin();
+			
+			
+			
+			try {
+				Statement st=conn1.createStatement();
+				
+				String sql = "";
+				if(!admin){
+					sql = "SELECT IDREPRESENTANTE FROM COMPANIASTEATRO WHERE ID="+id.toString();
+					PreparedStatement prepStmt = conn.prepareStatement(sql);
+					System.out.println(sql);
+					recursos.add(prepStmt);
+					ResultSet res = prepStmt.executeQuery();
+					if(!res.next())throw new Exception("La compa��a especificada no existe");
+					if(res.getLong("IDREPRESENTATNE")!=idRepresentante)throw new Exception("El operario especificado no tiene los permisos sobre esta compa��a");
+				}
+
+				sql = "SELECT ID, NOMBRE FROM COMPANIASTEATRO WHERE ID="+id;
+				PreparedStatement prepStmt = conn.prepareStatement(sql);
+				System.out.println(sql);
+				recursos.add(prepStmt);
+				ResultSet res = prepStmt.executeQuery();
+				if(!res.next())throw new Exception("La compa��a especificada no existe");
+				consul.setId(res.getLong("ID"));
+				consul.setNombre(res.getString("NOMBRE"));
+
+				sql = "SELECT E.ID, E.NOMBRE, COUNT(VENDIDO) AS ASISTOT "
+						+"FROM (((COMPANIASTEATRO C INNER JOIN PRESENTAN P ON C.ID=P.IDCOMPANIA)"
+						+"INNER JOIN ESPECTACULOS E ON P.IDESPECTACULO=E.ID) "
+						+"INNER JOIN FUNCIONES F ON E.ID=F.IDESPECTACULO) "
+						+"INNER JOIN BOLETAS B ON B.IDFUNCION = F.ID WHERE VENDIDO='Y' AND C.ID= "+id.toString()
+						+" GROUP BY (E.ID,E.NOMBRE) ORDER BY E.ID";
+				prepStmt = conn.prepareStatement(sql);
+				System.out.println(sql);
+				recursos.add(prepStmt);
+				ResultSet asisTotal = prepStmt.executeQuery();
+
+				sql = "SELECT E.ID, E.NOMBRE, COUNT(VENDIDO) AS REGISTRADOS "
+						+"FROM (((COMPANIASTEATRO C INNER JOIN PRESENTAN P ON C.ID=P.IDCOMPANIA)"
+						+"INNER JOIN ESPECTACULOS E ON P.IDESPECTACULO=E.ID) "
+						+"INNER JOIN FUNCIONES F ON E.ID=F.IDESPECTACULO) "
+						+"INNER JOIN BOLETAS B ON B.IDFUNCION = F.ID WHERE VENDIDO='Y' AND B.IDUSUARIO IS NOT NULL AND C.ID = "+id.toString()
+						+" GROUP BY (E.ID,E.NOMBRE)ORDER BY E.ID";
+				prepStmt = conn.prepareStatement(sql);
+				System.out.println(sql);
+				recursos.add(prepStmt);
+				ResultSet asisRegistrados = prepStmt.executeQuery();
+
+				sql = "SELECT E.ID, E.NOMBRE, SUM(PRECIO) AS GENERADO "
+						+"FROM ((((COMPANIASTEATRO C INNER JOIN PRESENTAN P ON C.ID=P.IDCOMPANIA)"
+						+"INNER JOIN ESPECTACULOS E ON P.IDESPECTACULO=E.ID)" 
+						+"INNER JOIN FUNCIONES F ON E.ID=F.IDESPECTACULO)" 
+						+"INNER JOIN BOLETAS B ON B.IDFUNCION = F.ID)"
+						+"INNER JOIN LOCALIDADES L ON L.ID=B.IDLOCALIDAD WHERE VENDIDO='Y' AND C.ID = "+id.toString()
+						+" GROUP BY (E.ID, E.NOMBRE)ORDER BY E.ID";
+				prepStmt = conn.prepareStatement(sql);
+				System.out.println(sql);
+				recursos.add(prepStmt);
+				ResultSet generado = prepStmt.executeQuery();
+
+				sql = "CREATE VIEW TOTAL AS (SELECT E.ID, E.NOMBRE AS NOMESP, S.ID AS IDSITIO, S.NOMBRE, B.VENDIDO "
+						+"FROM (((((COMPANIASTEATRO C INNER JOIN PRESENTAN P ON C.ID=P.IDCOMPANIA)"
+						+"INNER JOIN ESPECTACULOS E ON P.IDESPECTACULO=E.ID))"
+						+"INNER JOIN FUNCIONES F ON E.ID=F.IDESPECTACULO)"
+						+"INNER JOIN BOLETAS B ON B.IDFUNCION = F.ID) " 
+						+"INNER JOIN SITIOS S ON S.ID=F.IDSITIO WHERE C.ID = "+id.toString()+")";
+				prepStmt = conn.prepareStatement(sql);
+				System.out.println(sql);
+				recursos.add(prepStmt);
+				prepStmt.executeQuery();
+
+				sql = "SELECT TOTAL.ID, TOTAL.IDSITIO,TOTAL.NOMESP, TOTAL.NOMBRE, "
+						+"(COUNT(VENDIDO)/(CASE WHEN (SELECT COUNT(*) FROM TOTAL)=0 THEN 1 "
+						+"ELSE (SELECT COUNT(*) FROM TOTAL) END)) AS PORCENTAJE FROM TOTAL WHERE VENDIDO='Y' " 
+						+"GROUP BY (TOTAL.ID, TOTAL.IDSITIO,TOTAL.NOMESP, TOTAL.NOMBRE)ORDER BY TOTAL.ID ";
+				prepStmt = conn.prepareStatement(sql);
+				System.out.println(sql);
+				recursos.add(prepStmt);
+				ResultSet porcentaje = prepStmt.executeQuery();
+
+				sql = "DROP VIEW TOTAL";
+				prepStmt = conn.prepareStatement(sql);
+				System.out.println(sql);
+				recursos.add(prepStmt);
+				prepStmt.executeQuery();
+
+				while(asisTotal.next()){
+					CompaniaEspectaculo espectaculo = new CompaniaEspectaculo();
+					espectaculo.setId(asisTotal.getLong("ID"));
+					espectaculo.setNombre(asisTotal.getString("NOMBRE"));
+					espectaculo.setAsistTotal(asisTotal.getInt("ASISTOT"));
+
+					if(!asisRegistrados.next())throw new Exception("Something went wrong");
+					espectaculo.setAsistRegistrados(asisRegistrados.getInt("REGISTRADOS"));
+
+					if(!generado.next())throw new Exception("Something went wrong");
+					espectaculo.setDineroRecaudado(generado.getDouble("GENERADO"));
+
+					consul.addEspectaculo(espectaculo);
+				}
+
+				List<CompaniaEspectaculo> esp = consul.getEspectaculos();
+				int i = 0;
+				while(porcentaje.next())
+				{
+					if(esp.get(i).getNombre().equals(porcentaje.getString("NOMESP")))
+						esp.get(i).addSitio(new CompaniaSitio(porcentaje.getLong("IDSITIO"),porcentaje.getString("NOMBRE"),porcentaje.getDouble("PORCENTAJE")));
+					else if(esp.get(++i).getNombre().equals(porcentaje.getString("NOMESP")))
+						esp.get(i).addSitio(new CompaniaSitio(porcentaje.getLong("IDSITIO"),porcentaje.getString("NOMBRE"),porcentaje.getDouble("PORCENTAJE")));
+					else throw new Exception("Something went wrong");
+				}
+
+				
+				
+				st.close();
+	
+				
+			} catch (SQLException e) {
+				utx.setRollbackOnly();
+			}
+
+			try {
+				Statement st=conn2.createStatement();
+				String sql="";
+				System.out.println(sql);
+				int num=st.executeUpdate(sql);
+				System.out.println("Se modificaron "+num+" tuplas-Conexión 2");
+				st.close();
+				
+			} catch (SQLException e) {
+				utx.setRollbackOnly();
+			}
+
+			try {
+				Statement st=conn3.createStatement();
+				String sql="";
+				System.out.println(sql);
+				int num=st.executeUpdate(sql);
+				System.out.println("Se modificaron "+num+" tuplas-Conexión 3");
+				st.close();
+				
+			} catch (SQLException e) {
+				utx.setRollbackOnly();
+			}
+			
+			utx.commit();
+			cerrarConexiones();
+			
+			
+			}
+			catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+		} catch (NamingException e) {
+			
+			e.printStackTrace();
+		}
+		
+		return rentabilidad;
+		
+	}
 
 	private void cerrarConexiones() throws SQLException {
 		conn1.close();
